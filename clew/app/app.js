b8e32baa219c2c186553b4a4befc7436f7acc52e
Kristina <39955438+yakshina@users.noreply.github.com>
2021-03-22 01:00:57 +0300

    Delete clew directory

--- clew/app/app.js
+++
@@ -1,6 +0,0 @@
-var header = document.getElementById('body');
-var footer = document.getElementById('body');
-
-header.insertAdjacentHTML('afterbegin', '<header id="header" class="header"><div class="wrapper"><div class="nav"><div class="logo"></div><ul class="menu"><li><a href="#" class="icon">&#9776;</a></li><li><a href="org.html">Организатор</a></li><li><a href="guest.html">Гости</a></li><li><a href="stends.html">Стенды</a></li><li><a href="#">Математическая модель</a></li></ul></div></div></header>');
-footer.insertAdjacentHTML('beforeend', '<footer id="footer" class = "footer"><div class="wrapper"><section class="nav social"><div class="logo"></div><div class="menu"><ul  class = ""><li><a href="#" class = "facebook"></a></li><li><a href="#" class = "instagram"></a></li><li><a href="#" class = "pinterest"></a></li><li><a href="#" class = "telegram"></a></li></ul></div></section></div></footer>');
-    
